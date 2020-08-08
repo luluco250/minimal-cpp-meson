d817074878eca6a8e78bcd48c5e4af0c2c4c8b36
Lucas Melo <luluco250@gmail.com>
2020-08-08 20:52:01 -0300

    Added example documentation to the header.

--- Source/main.hpp
+++ Source/main.hpp
@@ -1,8 +1,22 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+/**
+ * Print "Hello World".
+ */
 void hello();
 
+/**
+ * Throw an std::runtime_error sometimes.
+ */
 void once_in_a_blue_moon();
+
+/**
+ * "Roll a die" and get the result.
+ *
+ * @param sides The number of sides of the die.
+ * @returns A random integer value between 1 and sides.
+ */
+int roll(int sides);
 
 #endif // Include guard.
